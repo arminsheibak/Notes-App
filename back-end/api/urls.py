7d54acc27c51f4a4938f208d3736cb5bdb967a02
Armin <armin2003sheibak@gmail.com>
2024-08-05 14:46:55 +0330

    build Notes api

--- back-end/api/urls.py
+++ back-end/api/urls.py
@@ -1,10 +1,15 @@
 from django.urls import include, path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.routers import DefaultRouter
 from . import views
+
+router = DefaultRouter()
+router.register('notes', views.NoteViewSet, basename='notes')
 
 urlpatterns = [
     path('users/register/', views.CreateUserView.as_view(), name='register' ),
     path('token/', TokenObtainPairView.as_view(), name='tokens'),
     path('token/refresh', TokenRefreshView.as_view(), name='refresh'),
     path('api=auth/', include('rest_framework.urls')),
+    path('', include(router.urls))
 ]
--- back-end/api/views.py
+++ back-end/api/views.py
@@ -1,9 +1,23 @@
 from django.contrib.auth import get_user_model
 from rest_framework.generics import CreateAPIView
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import CreateModelMixin, ListModelMixin, DestroyModelMixin
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSerializer
+from .models import Note
 
 class CreateUserView(CreateAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+class NoteViewSet(ListModelMixin, CreateModelMixin, DestroyModelMixin, GenericViewSet):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+    
+    def perform_create(self, serializer):
+        return serializer.save(author=self.request.user)
